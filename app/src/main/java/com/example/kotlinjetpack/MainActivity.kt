e7e3ae7cea05a518143a006f3d5e8808243863fd
SEBASTIN <BasimallaSebastin@gmail.com>
2022-11-15 22:01:18 -0500

    Commit Three

--- app/src/main/java/com/example/kotlinjetpack/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjetpack/MainActivity.kt
@@ -1,23 +1,61 @@
 package com.example.kotlinjetpack
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlinjetpack.ui.theme.KotlinJetPackTheme
+import androidx.compose.ui.unit.dp
+import com.example.kotlinjetpack.compose.TvShowListItem
+import com.example.kotlinjetpack.data.TvShowList
+import com.example.kotlinjetpack.model.TvShow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+        DisplayTvShows{
+                startActivity(InfoActivity.intent(this,it))
+            }
         }
     }
 }
+
+
+@Composable
+fun DisplayTvShows(selectedItem: (TvShow) -> Unit){
+    val tvShows = remember { TvShowList.tvShows }
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(
+            items = tvShows,
+            itemContent = {
+                TvShowListItem(tvShow = it, selectedItem = selectedItem)
+            }
+        )
+    }
+}
+
+
+
 
